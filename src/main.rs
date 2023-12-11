3483b4c3ed64eb71ed88d949677e308400a4b13d
Morgan Jones <me@morganlabs.dev>
2023-12-11 16:20:33 +0000

    feat: Looping function

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::Write; // <--- bring flush() into scope
 
 fn main() {
     // Generate a Random Number
-    let rand_num = rand::thread_rng().gen_range(1..=100);
+    let rand_num: u8 = rand::thread_rng().gen_range(1..=100);
     dbg!(rand_num);
 
     println!("Welcome to the Guessing Game!");
@@ -15,14 +15,39 @@ fn main() {
     let name = prompt("What's your name?");
     println!("\nWelcome to the Guessing Game, {}.", name);
 
-    let guess = prompt("What number am I thinking of?");
-    let guess: u32 = guess.parse().expect("Could not parse guess to type u32");
-    dbg!(guess);
+    let mut attempts: u8 = 1;
+    while attempts <= 3 {
+        let ans = take_guess(rand_num, attempts);
+
+        match ans.cmp(&0) {
+            Ordering::Less => {
+                if attempts != 3 {
+                    println!("Too low! Try again.");
+                };
+            }
+            Ordering::Greater => {
+                if attempts != 3 {
+                    println!("Too high! Try again.");
+                };
+            }
+            Ordering::Equal => {
+                println!("Correct! The number was {}", rand_num);
+                return;
+            }
+        }
+        attempts += 1;
+    }
+    println!("Too bad! The number was {}", rand_num);
+}
+
+fn take_guess(rand_num: u8, attempt: u8) -> i8 {
+    let guess = prompt(format!("What number am I thinking of? (Attempt {}/3)", attempt).as_str());
+    let guess: u8 = guess.parse().expect("Could not parse guess to type u32");
 
     match guess.cmp(&rand_num) {
-        Ordering::Less => println!("Too low!"),
-        Ordering::Greater => println!("Too high!"),
-        Ordering::Equal => println!("Correct!"),
+        Ordering::Less => -1,
+        Ordering::Greater => 1,
+        Ordering::Equal => 0,
     }
 }
 
