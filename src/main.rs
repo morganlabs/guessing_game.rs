24d3bd3786782edea6c37e0c91ae7c10ec4065e0
Morgan Jones <me@morganlabs.dev>
2023-12-11 15:50:53 +0000

    feat: Ask user for their name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+use std::io::Write; // <--- bring flush() into scope
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Guessing Game!");
+
+    let name = prompt("What's your name?");
+    println!("Welcome to the Guessing Game, {}.", name);
+}
+
+fn prompt(prompt: &str) -> String {
+    print!("{}: ", prompt);
+
+    // Flush the buffer
+    io::stdout().flush().expect("Failed to flush the stdout.");
+
+    // Get the answer
+    let mut ans = String::new();
+    io::stdin()
+        .read_line(&mut ans)
+        .expect("Failed to read line.");
+
+    return ans.trim().to_string();
 }
